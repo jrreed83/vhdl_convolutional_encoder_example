36146d44fa4b5bc38fd34401507fb8b26931d8fb
jrreed83 <joeyrreed@gmail.com>
2025-02-22 05:42:15 -0800

    jrr

--- convenc_tb.vhd
+++ convenc_tb.vhd
@@ -2,6 +2,9 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
+--library OSVVM ; 
+--  context OSVVM.OsvvmContext ;  
+
 library osvvm;
   use osvvm.ClockResetPkg.all;
   use osvvm.RandomPkg.all;
